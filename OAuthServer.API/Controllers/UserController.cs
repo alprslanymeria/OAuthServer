2f5bfa339fa39352fbf3895ca0c395d62101d6ad
alparslan <alparslanorgnl@gmail.com>
2025-11-24 23:05:10 +0300

    UserController created

---
+++ OAuthServer.API/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OAuthServer.Core.DTOs;
+using OAuthServer.Core.Services;
+
+namespace OAuthServer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(
+
+        IUserService userService) : BaseController
+    {
+        private readonly IUserService _userService = userService;
+
+        // TESTED WITH POSTMAN ✅
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(SignUpDto signUpDto)
+        {
+            var result = await _userService.CreateUserAsync(signUpDto);
+
+            return ActionResultInstance(result);
+        }
+    }
+}
