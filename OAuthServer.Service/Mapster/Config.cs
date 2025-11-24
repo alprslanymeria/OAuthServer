9cae7ce97df054c1e9f5c08f50351e700e6a9206
alparslan <alparslanorgnl@gmail.com>
2025-11-24 22:51:04 +0300

    Mapster config created

---
+++ OAuthServer.Service/Mapster/Config.cs
@@ -0,0 +1,18 @@
+﻿using Mapster;
+using OAuthServer.Core.DTOs;
+using OAuthServer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OAuthServer.Service.Mapster
+{
+    internal class MapperConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            // ENTITY TO DTO
+            config.NewConfig<User, UserDto>();
+        }
+    }
+}
