5bb2a444d1695a3ad9fcb505336326a5923a891f
alparslan <alparslanorgnl@gmail.com>
2025-11-24 22:54:11 +0300

    SignService created

---
+++ OAuthServer.Service/Services/SignService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OAuthServer.Service.Services
+{
+    public static class SignService
+    {
+        // TOKEN İMZALAMA İŞLEMLERİNDE KULLANMAK İÇİN SYMETRIC KEY OLUŞTURUYORUZ.
+        public static SecurityKey GetSymetricKey(string securityKey)
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+        }
+    }
+}
