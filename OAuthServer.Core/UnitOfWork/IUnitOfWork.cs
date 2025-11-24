0355169057d0c3395b576cad8c7395620d999bd4
alparslan <alparslanorgnl@gmail.com>
2025-11-24 22:30:19 +0300

    IUnitOfWork created

---
+++ OAuthServer.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OAuthServer.Core.UnitOfWork
+{
+    // AMACIMIIZ DATABASE İŞLEMLERİNİ TEK BİR TRANSACTION ÜZERİNDEN GERÇEKLEŞTİRMEK.
+    public interface IUnitOfWork
+    {
+        // TASK ASENKRON İŞLEMLERDE VOID'E KARŞILIK GELİR.
+        Task CommitAsync();
+        void Commit();
+    }
+}
