5563fccaf4c12a4501d7abc8aff553dea1487364
alparslan <alparslanorgnl@gmail.com>
2025-11-24 23:00:09 +0300

    Authentication & User services implemented

---
+++ OAuthServer.Service/Services/UserService.cs
@@ -0,0 +1,48 @@
+﻿using MapsterMapper;
+using Microsoft.AspNetCore.Identity;
+using OAuthServer.Core.DTOs;
+using OAuthServer.Core.Helper;
+using OAuthServer.Core.Models;
+using OAuthServer.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OAuthServer.Service.Services
+{
+    public class UserService(
+
+        UserManager<User> userManager,
+        IMapper mapper) : IUserService
+    {
+        private readonly UserManager<User> _userManager = userManager;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<Response<UserDto>> CreateUserAsync(SignUpDto signUpDto)
+        {
+            var user = new User
+            {
+                UserName = signUpDto.Username,
+                Email = signUpDto.Email,
+                NativeLanguageId = signUpDto.NativeLanguageId,
+            };
+
+            var result = await _userManager.CreateAsync(user, signUpDto.Password);
+
+            if (!result.Succeeded)
+            {
+                // GET ERROR MESSAGES
+                var errors = result.Errors.Select(e => e.Description).ToList();
+
+                var errorDto = new ErrorDto(errors, true);
+
+                return Response<UserDto>.Fail(errorDto, 400);
+            }
+
+            // USER MAP TO USERDTO
+            var userDto = _mapper.Map<UserDto>(user);
+
+            return Response<UserDto>.Success(201);
+        }
+    }
+}
