8fd5ff0f4cc7ce72716cd5934b7981679e9f1da4
alparslan <alparslanorgnl@gmail.com>
2025-11-24 22:27:45 +0300

    ITokenServiceCreated

---
+++ OAuthServer.Core/Services/ITokenService.cs
@@ -0,0 +1,26 @@
+﻿using OAuthServer.Core.DTOs;
+using OAuthServer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OAuthServer.Core.Services
+{
+    public interface ITokenService
+    {
+        // BURDA BULUNAN METOTLARIN IMPLAMENTASYONU SERVICE KATMANINDA YAPILIR.
+        // BURADA BULUNAN METOTLAR SERVICE VEYA PRESENTATION (API) KATMANINDA KULLANILABİLİR.
+        // METOTLARDAN DÖNEN VERİ İSE SERVICE VEYA PRESENTATION (API) KATMANINDA KULLANILABİLİR.
+
+        TokenDto CreateToken(User user);
+
+        //ClientTokenDto CreateTokenByClient(Client client)
+
+        //public class Client
+        //{
+        //    public string Id { get; set; } = null!;
+        //    public string Secret { get; set; } = null!;
+        //    public List<string> Audiences { get; set; } = null!;
+        //}
+    }
+}
