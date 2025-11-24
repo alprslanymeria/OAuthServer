bf017263927acf5220223f689bfb220ebc8de11b
alparslan <alparslanorgnl@gmail.com>
2025-11-24 22:47:17 +0300

    IUnitOfWork implemented

---
+++ OAuthServer.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using OAuthServer.Core.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OAuthServer.Data.UnitOfWork
+{
+    public class UnitOfWork(AppDbContext context) : IUnitOfWork
+    {
+        private readonly AppDbContext _context = context;
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
