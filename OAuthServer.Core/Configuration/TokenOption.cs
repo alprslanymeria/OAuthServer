c4989644e53137301cab3120a92759906b069ca7
alparslan <alparslanorgnl@gmail.com>
2025-11-24 22:16:58 +0300

    TokenOption created

---
+++ OAuthServer.Core/Configuration/TokenOption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OAuthServer.Core.Configuration
+{
+    public class TokenOption
+    {
+        public List<String> Audience { get; set; }
+        public string Issuer { get; set; } = null!;
+        public int AccessTokenExpiration { get; set; }
+        public int RefreshTokenExpiration { get; set; }
+        public string SecurityKey { get; set; } = null!;
+    }
+}
