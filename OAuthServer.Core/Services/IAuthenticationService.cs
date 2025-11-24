81964f39cc5ef7baf047db495da26e55b120040b
alparslan <alparslanorgnl@gmail.com>
2025-11-24 22:29:24 +0300

    IAuthenticationService created

---
+++ OAuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,23 @@
+﻿using OAuthServer.Core.DTOs;
+using OAuthServer.Core.Helper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OAuthServer.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        // BURDA BULUNAN METOTLARIN IMPLAMENTASYONU SERVICE KATMANINDA YAPILIR.
+        // BURADA BULUNAN METOTLAR SERVICE VEYA PRESENTATION (API) KATMANINDA KULLANILABİLİR.
+        // METOTLARDAN DÖNEN VERİ İSE SERVICE VEYA PRESENTATION (API) KATMANINDA KULLANILABİLİR.
+
+        Task<Response<TokenDto>> CreateTokenAsync(SignInDto signInDto);
+
+        Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+
+        Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
+
+        //Response<ClientTokenDto> CreateTokenByClient(ClientSignInDto clientSignInDto);
+    }
+}
