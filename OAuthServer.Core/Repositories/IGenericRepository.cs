a4603980303d2dffcc0ccaecde4e4bbf8c7d1d5c
alparslan <alparslanorgnl@gmail.com>
2025-11-24 22:25:28 +0300

    IGenericRepository created

---
+++ OAuthServer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace OAuthServer.Core.Repositories
+{
+    // BURDA BULUNAN METOTLARIN IMPLAMENTASYONU DATA KATMANINDA YAPILIR.
+    // BURADA BULUNAN METOTLAR DATA VEYA SERVICE KATMANINDA KULLANILABİLİR.
+    // METOTLARDAN DÖNEN VERİ İSE DATA VEYA SERVICE KATMANINDA KULLANILABİLİR.
+
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        // ENTITY'LER İÇİN YAPILAN GENEL İŞLEMLER YANİ CRUD İŞLEMLERİ BURADA TANIMLANACAK.
+        // KÜÇÜK VE ORTA BAZLI PROJELERDE GENERİC REPOSİTORY PATTERN YETERLİDİR. DAHA BÜYÜK SEVİYELERDE DDD KULLANILIR.
+
+        Task<TEntity> GetEntityByIdAsync(int id);
+
+        // ToList() METODUNU ÇAĞIRANA KADAR IQueryable VERİTABANINA YANSIMAZ.
+        IQueryable<TEntity> GetAllAsync();
+
+        IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
+
+        Task AddAsync(TEntity entity);
+
+        void Remove(TEntity entity);
+
+        TEntity Update(TEntity entity);
+    }
+}
