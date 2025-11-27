dbbfa8fe14354f2b9ea59b79935fb54a50f08e80
alparslan <alparslanorgnl@gmail.com>
2025-11-27 20:50:03 +0300

    option pattern applied for connection string

---
+++ OAuthServer.Data/Configurations/ConnStringOption.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OAuthServer.Data.Configurations;
+public class ConnStringOption
+{
+    public const string Key = "ConnectionStrings";
+    public string SqlServer { get; set; } = default!;
+}
