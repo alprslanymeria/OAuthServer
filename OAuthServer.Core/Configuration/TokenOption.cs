930c2a02b935585cad8277973962fe1a2792cbc3
alparslan <alparslanorgnl@gmail.com>
2025-11-27 21:52:24 +0300

    token option Key prop added

--- OAuthServer.Core/Configuration/TokenOption.cs
+++ OAuthServer.Core/Configuration/TokenOption.cs
@@ -2,6 +2,7 @@
 
 public class TokenOption
 {
+    public const string Key = "TokenOptions";
     public List<String> Audience { get; set; } = default!;
     public string Issuer { get; set; } = default!;
     public int AccessTokenExpiration { get; set; }
